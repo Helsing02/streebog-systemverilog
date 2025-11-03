00609182dd3199ce44cd3b736184ce412d6d523d
Helsing <ivancool2222@gmail.com>
2025-11-03 14:52:19 +0700

    add g transform

---
+++ rtl/g_transform.sv
@@ -0,0 +1,241 @@
+module g_transform (
+    input logic clk,                        // Clock
+    input logic rst_n,                      // Synchronous reset active low
+
+    input logic [511:0] i_h_data,           // Input h signal in gN(h, m)
+    input logic [511:0] i_N_data,           // Input N signal in gN(h, m)
+
+    input  logic [511:0] s_axis_m_tdata,    // Input m signal in gN(h, m) (message block)
+    input  logic         s_axis_m_tvalid,   // Input signal indicating the validity of a message
+    output logic         s_axis_m_tready,   // Output signal indicating the module's readiness to receive a message
+
+    output logic [511:0] o_h_data,          // Output signal of the calculation result gN(h, m)
+    output logic o_h_valid                  // Output signal about the validity of the calculation result
+);
+
+// 12 constants of the algorithm
+const logic [511:0] C[0:11] = {
+    {
+        8'hb1, 8'h08, 8'h5b, 8'hda, 8'h1e, 8'hca, 8'hda, 8'he9,
+        8'heb, 8'hcb, 8'h2f, 8'h81, 8'hc0, 8'h65, 8'h7c, 8'h1f,
+        8'h2f, 8'h6a, 8'h76, 8'h43, 8'h2e, 8'h45, 8'hd0, 8'h16,
+        8'h71, 8'h4e, 8'hb8, 8'h8d, 8'h75, 8'h85, 8'hc4, 8'hfc,
+        8'h4b, 8'h7c, 8'he0, 8'h91, 8'h92, 8'h67, 8'h69, 8'h01,
+        8'ha2, 8'h42, 8'h2a, 8'h08, 8'ha4, 8'h60, 8'hd3, 8'h15,
+        8'h05, 8'h76, 8'h74, 8'h36, 8'hcc, 8'h74, 8'h4d, 8'h23,
+        8'hdd, 8'h80, 8'h65, 8'h59, 8'hf2, 8'ha6, 8'h45, 8'h07
+    },
+    {
+        8'h6f, 8'ha3, 8'hb5, 8'h8a, 8'ha9, 8'h9d, 8'h2f, 8'h1a,
+        8'h4f, 8'he3, 8'h9d, 8'h46, 8'h0f, 8'h70, 8'hb5, 8'hd7,
+        8'hf3, 8'hfe, 8'hea, 8'h72, 8'h0a, 8'h23, 8'h2b, 8'h98,
+        8'h61, 8'hd5, 8'h5e, 8'h0f, 8'h16, 8'hb5, 8'h01, 8'h31,
+        8'h9a, 8'hb5, 8'h17, 8'h6b, 8'h12, 8'hd6, 8'h99, 8'h58,
+        8'h5c, 8'hb5, 8'h61, 8'hc2, 8'hdb, 8'h0a, 8'ha7, 8'hca,
+        8'h55, 8'hdd, 8'ha2, 8'h1b, 8'hd7, 8'hcb, 8'hcd, 8'h56,
+        8'he6, 8'h79, 8'h04, 8'h70, 8'h21, 8'hb1, 8'h9b, 8'hb7
+    },
+    {
+        8'hf5, 8'h74, 8'hdc, 8'hac, 8'h2b, 8'hce, 8'h2f, 8'hc7,
+        8'h0a, 8'h39, 8'hfc, 8'h28, 8'h6a, 8'h3d, 8'h84, 8'h35,
+        8'h06, 8'hf1, 8'h5e, 8'h5f, 8'h52, 8'h9c, 8'h1f, 8'h8b,
+        8'hf2, 8'hea, 8'h75, 8'h14, 8'hb1, 8'h29, 8'h7b, 8'h7b,
+        8'hd3, 8'he2, 8'h0f, 8'he4, 8'h90, 8'h35, 8'h9e, 8'hb1,
+        8'hc1, 8'hc9, 8'h3a, 8'h37, 8'h60, 8'h62, 8'hdb, 8'h09,
+        8'hc2, 8'hb6, 8'hf4, 8'h43, 8'h86, 8'h7a, 8'hdb, 8'h31,
+        8'h99, 8'h1e, 8'h96, 8'hf5, 8'h0a, 8'hba, 8'h0a, 8'hb2
+    },
+    {
+        8'hef, 8'h1f, 8'hdf, 8'hb3, 8'he8, 8'h15, 8'h66, 8'hd2,
+        8'hf9, 8'h48, 8'he1, 8'ha0, 8'h5d, 8'h71, 8'he4, 8'hdd,
+        8'h48, 8'h8e, 8'h85, 8'h7e, 8'h33, 8'h5c, 8'h3c, 8'h7d,
+        8'h9d, 8'h72, 8'h1c, 8'had, 8'h68, 8'h5e, 8'h35, 8'h3f,
+        8'ha9, 8'hd7, 8'h2c, 8'h82, 8'hed, 8'h03, 8'hd6, 8'h75,
+        8'hd8, 8'hb7, 8'h13, 8'h33, 8'h93, 8'h52, 8'h03, 8'hbe,
+        8'h34, 8'h53, 8'hea, 8'ha1, 8'h93, 8'he8, 8'h37, 8'hf1,
+        8'h22, 8'h0c, 8'hbe, 8'hbc, 8'h84, 8'he3, 8'hd1, 8'h2e
+    },
+    {
+        8'h4b, 8'hea, 8'h6b, 8'hac, 8'had, 8'h47, 8'h47, 8'h99,
+        8'h9a, 8'h3f, 8'h41, 8'h0c, 8'h6c, 8'ha9, 8'h23, 8'h63,
+        8'h7f, 8'h15, 8'h1c, 8'h1f, 8'h16, 8'h86, 8'h10, 8'h4a,
+        8'h35, 8'h9e, 8'h35, 8'hd7, 8'h80, 8'h0f, 8'hff, 8'hbd,
+        8'hbf, 8'hcd, 8'h17, 8'h47, 8'h25, 8'h3a, 8'hf5, 8'ha3,
+        8'hdf, 8'hff, 8'h00, 8'hb7, 8'h23, 8'h27, 8'h1a, 8'h16,
+        8'h7a, 8'h56, 8'ha2, 8'h7e, 8'ha9, 8'hea, 8'h63, 8'hf5,
+        8'h60, 8'h17, 8'h58, 8'hfd, 8'h7c, 8'h6c, 8'hfe, 8'h57
+    },
+    {
+        8'hae, 8'h4f, 8'hae, 8'hae, 8'h1d, 8'h3a, 8'hd3, 8'hd9,
+        8'h6f, 8'ha4, 8'hc3, 8'h3b, 8'h7a, 8'h30, 8'h39, 8'hc0,
+        8'h2d, 8'h66, 8'hc4, 8'hf9, 8'h51, 8'h42, 8'ha4, 8'h6c,
+        8'h18, 8'h7f, 8'h9a, 8'hb4, 8'h9a, 8'hf0, 8'h8e, 8'hc6,
+        8'hcf, 8'hfa, 8'ha6, 8'hb7, 8'h1c, 8'h9a, 8'hb7, 8'hb4,
+        8'h0a, 8'hf2, 8'h1f, 8'h66, 8'hc2, 8'hbe, 8'hc6, 8'hb6,
+        8'hbf, 8'h71, 8'hc5, 8'h72, 8'h36, 8'h90, 8'h4f, 8'h35,
+        8'hfa, 8'h68, 8'h40, 8'h7a, 8'h46, 8'h64, 8'h7d, 8'h6e
+    },
+    {
+        8'hf4, 8'hc7, 8'h0e, 8'h16, 8'hee, 8'haa, 8'hc5, 8'hec,
+        8'h51, 8'hac, 8'h86, 8'hfe, 8'hbf, 8'h24, 8'h09, 8'h54,
+        8'h39, 8'h9e, 8'hc6, 8'hc7, 8'he6, 8'hbf, 8'h87, 8'hc9,
+        8'hd3, 8'h47, 8'h3e, 8'h33, 8'h19, 8'h7a, 8'h93, 8'hc9,
+        8'h09, 8'h92, 8'hab, 8'hc5, 8'h2d, 8'h82, 8'h2c, 8'h37,
+        8'h06, 8'h47, 8'h69, 8'h83, 8'h28, 8'h4a, 8'h05, 8'h04,
+        8'h35, 8'h17, 8'h45, 8'h4c, 8'ha2, 8'h3c, 8'h4a, 8'hf3,
+        8'h88, 8'h86, 8'h56, 8'h4d, 8'h3a, 8'h14, 8'hd4, 8'h93
+    },
+    {
+        8'h9b, 8'h1f, 8'h5b, 8'h42, 8'h4d, 8'h93, 8'hc9, 8'ha7,
+        8'h03, 8'he7, 8'haa, 8'h02, 8'h0c, 8'h6e, 8'h41, 8'h41,
+        8'h4e, 8'hb7, 8'hf8, 8'h71, 8'h9c, 8'h36, 8'hde, 8'h1e,
+        8'h89, 8'hb4, 8'h44, 8'h3b, 8'h4d, 8'hdb, 8'hc4, 8'h9a,
+        8'hf4, 8'h89, 8'h2b, 8'hcb, 8'h92, 8'h9b, 8'h06, 8'h90,
+        8'h69, 8'hd1, 8'h8d, 8'h2b, 8'hd1, 8'ha5, 8'hc4, 8'h2f,
+        8'h36, 8'hac, 8'hc2, 8'h35, 8'h59, 8'h51, 8'ha8, 8'hd9,
+        8'ha4, 8'h7f, 8'h0d, 8'hd4, 8'hbf, 8'h02, 8'he7, 8'h1e
+    },
+    {
+        8'h37, 8'h8f, 8'h5a, 8'h54, 8'h16, 8'h31, 8'h22, 8'h9b,
+        8'h94, 8'h4c, 8'h9a, 8'hd8, 8'hec, 8'h16, 8'h5f, 8'hde,
+        8'h3a, 8'h7d, 8'h3a, 8'h1b, 8'h25, 8'h89, 8'h42, 8'h24,
+        8'h3c, 8'hd9, 8'h55, 8'hb7, 8'he0, 8'h0d, 8'h09, 8'h84,
+        8'h80, 8'h0a, 8'h44, 8'h0b, 8'hdb, 8'hb2, 8'hce, 8'hb1,
+        8'h7b, 8'h2b, 8'h8a, 8'h9a, 8'ha6, 8'h07, 8'h9c, 8'h54,
+        8'h0e, 8'h38, 8'hdc, 8'h92, 8'hcb, 8'h1f, 8'h2a, 8'h60,
+        8'h72, 8'h61, 8'h44, 8'h51, 8'h83, 8'h23, 8'h5a, 8'hdb
+    },
+    {
+        8'hab, 8'hbe, 8'hde, 8'ha6, 8'h80, 8'h05, 8'h6f, 8'h52,
+        8'h38, 8'h2a, 8'he5, 8'h48, 8'hb2, 8'he4, 8'hf3, 8'hf3,
+        8'h89, 8'h41, 8'he7, 8'h1c, 8'hff, 8'h8a, 8'h78, 8'hdb,
+        8'h1f, 8'hff, 8'he1, 8'h8a, 8'h1b, 8'h33, 8'h61, 8'h03,
+        8'h9f, 8'he7, 8'h67, 8'h02, 8'haf, 8'h69, 8'h33, 8'h4b,
+        8'h7a, 8'h1e, 8'h6c, 8'h30, 8'h3b, 8'h76, 8'h52, 8'hf4,
+        8'h36, 8'h98, 8'hfa, 8'hd1, 8'h15, 8'h3b, 8'hb6, 8'hc3,
+        8'h74, 8'hb4, 8'hc7, 8'hfb, 8'h98, 8'h45, 8'h9c, 8'hed
+    },
+    {
+        8'h7b, 8'hcd, 8'h9e, 8'hd0, 8'hef, 8'hc8, 8'h89, 8'hfb,
+        8'h30, 8'h02, 8'hc6, 8'hcd, 8'h63, 8'h5a, 8'hfe, 8'h94,
+        8'hd8, 8'hfa, 8'h6b, 8'hbb, 8'heb, 8'hab, 8'h07, 8'h61,
+        8'h20, 8'h01, 8'h80, 8'h21, 8'h14, 8'h84, 8'h66, 8'h79,
+        8'h8a, 8'h1d, 8'h71, 8'hef, 8'hea, 8'h48, 8'hb9, 8'hca,
+        8'hef, 8'hba, 8'hcd, 8'h1d, 8'h7d, 8'h47, 8'h6e, 8'h98,
+        8'hde, 8'ha2, 8'h59, 8'h4a, 8'hc0, 8'h6f, 8'hd8, 8'h5d,
+        8'h6b, 8'hca, 8'ha4, 8'hcd, 8'h81, 8'hf3, 8'h2d, 8'h1b
+    },
+    {
+        8'h37, 8'h8e, 8'he7, 8'h67, 8'hf1, 8'h16, 8'h31, 8'hba,
+        8'hd2, 8'h13, 8'h80, 8'hb0, 8'h04, 8'h49, 8'hb1, 8'h7a,
+        8'hcd, 8'ha4, 8'h3c, 8'h32, 8'hbc, 8'hdf, 8'h1d, 8'h77,
+        8'hf8, 8'h20, 8'h12, 8'hd4, 8'h30, 8'h21, 8'h9f, 8'h9b,
+        8'h5d, 8'h80, 8'hef, 8'h9d, 8'h18, 8'h91, 8'hcc, 8'h86,
+        8'he7, 8'h1d, 8'ha4, 8'haa, 8'h88, 8'he1, 8'h28, 8'h52,
+        8'hfa, 8'hf4, 8'h17, 8'hd5, 8'hd9, 8'hb2, 8'h1b, 8'h99,
+        8'h48, 8'hbc, 8'h92, 8'h4a, 8'hf1, 8'h1b, 8'hd7, 8'h20
+    }
+};
+
+// ======= G transform calculation FSM =======
+typedef enum logic [3:0] {
+    IDLE, S1,  S2,  S3,
+    S4,   S5,  S6,  S7,
+    S8,   S9,  S10, S11,
+    S12,  S13, READY
+} statetype;
+
+statetype state, nextstate;
+
+always_ff @(posedge clk) begin : proc_state
+    if(~rst_n) begin
+        state <= IDLE;
+    end else begin
+        state <= nextstate;
+    end
+end
+
+// Next state logic
+always_comb begin
+    case (state)
+        IDLE:    nextstate = (s_axis_m_tvalid == 1) ? S1 : IDLE;
+        S1:      nextstate = S2;
+        S2:      nextstate = S3;
+        S3:      nextstate = S4;
+        S4:      nextstate = S5;
+        S5:      nextstate = S6;
+        S6:      nextstate = S7;
+        S7:      nextstate = S8;
+        S8:      nextstate = S9;
+        S9:      nextstate = S10;
+        S10:     nextstate = S11;
+        S11:     nextstate = S12;
+        S12:     nextstate = S13;
+        S13:     nextstate = READY;
+        READY:   nextstate = IDLE;
+        default: nextstate = IDLE;
+    endcase
+end
+
+logic [511:0] key_reg;
+logic [511:0] m_reg;
+logic [511:0] m_xor_h_reg;
+
+logic [511:0] i_data_a_key;
+logic [511:0] i_data_b_key;
+logic [511:0] o_data_key;
+logic [511:0] o_data_m;
+
+
+always_ff @(posedge clk) begin : proc_key_reg
+    if(~rst_n) begin
+        key_reg <= '0;
+    end else begin
+        key_reg <= o_data_key;
+    end
+end
+
+always_ff @(posedge clk) begin : proc_m_reg
+    if(~rst_n) begin
+        m_reg <= '0;
+    end else begin
+        m_reg <= (nextstate == S1) ? s_axis_m_tdata : o_data_m;
+    end
+end
+
+always_ff @(posedge clk) begin : proc_m_xor_h_reg
+    if(~rst_n) begin
+        m_xor_h_reg <= '0;
+    end else if (nextstate == S1) begin
+        m_xor_h_reg <= s_axis_m_tdata ^ i_h_data;
+    end
+end
+
+always_ff @(posedge clk) begin : proc_o_h_data
+    if(~rst_n) begin
+        o_h_data <= '0;
+    end else if (nextstate == READY) begin
+        o_h_data <= key_reg ^ m_reg ^ m_xor_h_reg;
+    end
+end
+
+// Instantite two LPSX
+lpsx_transform lpsx_for_key (
+    .i_data_a(i_data_a_key),
+    .i_data_b(i_data_b_key),
+    .o_data  (o_data_key)
+);
+
+lpsx_transform lpsx_for_m (
+    .i_data_a(m_reg),
+    .i_data_b(key_reg),
+    .o_data  (o_data_m)
+);
+
+assign i_data_a_key = (state == IDLE) ? i_h_data : key_reg;
+assign i_data_b_key = (state == IDLE) ? i_N_data : C[state - 1];
+
+assign o_h_valid = (state == READY);
+assign s_axis_m_tready = (state == IDLE);
+
+endmodule : g_transform
---
+++ sim/g_transform/tb_g_transform.sv
@@ -0,0 +1,130 @@
+`resetall
+`timescale 1ns / 1ps
+
+module tb_g_transform # (
+    parameter ROUNDS = 100
+);
+
+// Import DPI functions - "load" C functions into SV
+import "DPI-C" function void stribog_g_transform(
+    input  byte unsigned N[64],
+    input  byte unsigned h[64],
+    input  byte unsigned m[64],
+    output byte unsigned res[64]
+);
+
+logic         clk;    // Clock
+logic         rst_n;  // Synchronous reset active low
+
+logic [511:0] i_h_data;
+logic [511:0] i_N_data;
+
+logic [511:0] s_axis_m_tdata;
+logic         s_axis_m_tvalid;
+logic         s_axis_m_tready;
+
+logic [511:0] o_h_data;
+logic         o_h_valid;
+
+// Initialize DUT
+g_transform dut (
+    .clk            (clk),
+    .rst_n          (rst_n),
+
+    .i_h_data       (i_h_data),
+    .i_N_data       (i_N_data),
+
+    .s_axis_m_tdata (s_axis_m_tdata),
+    .s_axis_m_tvalid(s_axis_m_tvalid),
+    .s_axis_m_tready(s_axis_m_tready),
+
+    .o_h_data       (o_h_data),
+    .o_h_valid      (o_h_valid)
+);
+
+// Expected data signals
+logic [511:0] expected_o_h_data;
+
+function logic [511:0] call_g_transform (
+    input logic [511:0] input_N,
+    input logic [511:0] input_h,
+    input logic [511:0] input_m
+);
+    byte unsigned N [64];
+    byte unsigned h [64];
+    byte unsigned m [64];
+    byte unsigned call_result [64];
+    logic [511:0] result;
+    begin
+        for (int i = 0; i < 64; i++) begin
+            N[i] = input_N[i*8 +: 8];
+            h[i] = input_h[i*8 +: 8];
+            m[i] = input_m[i*8 +: 8];
+        end
+        stribog_g_transform(N, h, m, call_result);
+        for (int i = 0; i < 64; i++) begin
+            result[i*8 +: 8] = call_result[i];
+        end
+        return result;
+    end
+endfunction
+
+initial begin
+    // Initialize signals
+    clk = 1'b0;
+    rst_n = 1'b1;
+    i_h_data = 512'b0;
+    i_N_data = 512'b0;
+    s_axis_m_tdata = 512'b0;
+    s_axis_m_tvalid = 1'b0;
+
+    #3;
+    rst_n = 0;
+    #10;
+    rst_n = 1;
+    // Test random vectors
+    for (int i = 0; i < ROUNDS; i++) begin : random_vectors_loop
+        while (~s_axis_m_tready) #10;
+
+        i_h_data = {
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom
+        };
+        i_N_data = {
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom
+        };
+        s_axis_m_tdata = {
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom,
+            $urandom, $urandom, $urandom, $urandom
+        };
+
+        expected_o_h_data = call_g_transform(i_N_data, i_h_data, s_axis_m_tdata);
+
+        s_axis_m_tvalid = 1;
+        #10;
+        s_axis_m_tvalid = 0;
+
+        while (o_h_valid != 1) #10;
+
+        assert (o_h_data == expected_o_h_data) else begin
+            $error("ASSERTION FAILED:\n dut_output = %h\n expected = %h", o_h_data, expected_o_h_data);
+            $stop;
+        end
+        #10;
+    end
+    $stop;
+
+end
+
+always begin
+    #5; clk = ~clk;
+end
+
+endmodule : tb_g_transform
